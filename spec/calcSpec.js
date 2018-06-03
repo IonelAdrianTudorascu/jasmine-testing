923cda12978ad9c543dbff0bbc7c73ab20adbb43
Ionel Adrian Tudorascu <adrian_tudorascu@yahoo.ro>
2018-06-03 14:51:23 +0000

    added a new function

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -7,5 +7,11 @@ describe("Calculator", function() {
         it("should not return 4 if the arguments given don't produce a sum of 4", function() {
             expect(addition(7, 19)).toBe(26);
         });
+
+        it("should have called the alert function if either number is undefined", function() {
+            spyOn(window, "alert");
+            addition("Hello");
+            expect(window.alert).toHaveBeenCalledWith("Both arguments must be numbers");
+        });
     });
 });
