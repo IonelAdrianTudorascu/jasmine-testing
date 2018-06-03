ef2bc64e3d62cf59591288101f68e8420a08cca3
Ionel Adrian Tudorascu <adrian_tudorascu@yahoo.ro>
2018-06-03 15:24:43 +0000

    added a new Calc function

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,17 +1,26 @@
 describe("Calculator", function() {
-    describe("Addition function", function() {
+
+    beforeEach(function() {
+        calc = new Calculator();
+    });
+
+    describe("Add method", function() {
         it("should take two arguments and return the sum of those two arguments", function() {
-            expect(addition(2, 2)).toBe(4);
+            calc.add(2);
+            calc.add(2);
+            expect(calc.value).toBe(4);
         });
 
         it("should not return 4 if the arguments given don't produce a sum of 4", function() {
-            expect(addition(7, 19)).toBe(26);
+            calc.add(7);
+            calc.add(19)
+            expect(calc.value).toBe(26);
         });
 
         it("should have called the alert function if either number is undefined", function() {
             spyOn(window, "alert");
-            addition("Hello");
-            expect(window.alert).toHaveBeenCalledWith("Both arguments must be numbers");
+            calc.add("Hello");
+            expect(window.alert).toHaveBeenCalledWith("Arguments must be numbers");
         });
     });
 });
